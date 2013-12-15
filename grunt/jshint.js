2f1318f495fb94c1e88f079537b7de98627c2414
solygen <frank.paczynski@solygen.de>
2013-12-15 08:08:27 +0100

    update package.json; use .jshintrc

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,27 +1,3 @@
 module.exports = {
-    files: ['src/*.js'],
-    //https://github.com/gruntjs/grunt-contrib-jshint/blob/master/docs/jshint-examples.md
-    options: {
-        bitwise: false,
-        browser: true,
-        debug: true,
-        devel: true,
-        eqeqeq: true,
-        evil: true,
-        forin: false,
-        immed: true,
-        loopfunc: false,
-        nomen: false,
-        onevar: false,
-        plusplus: false,
-        regexp: false,
-        regexdash: true,
-        shadow: true,
-        strict: true,
-        trailing: true,
-        undef: true,
-        validthis: true,
-        white: true,
-        predef: ['$']
-  }
+    files: ['**/*.js']
 };
